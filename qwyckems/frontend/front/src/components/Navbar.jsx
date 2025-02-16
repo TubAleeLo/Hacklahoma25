11bdb70344fde654bbdcc371844225179c3ce3fb
RomanXXII <romanbeames@gmail.com>
2025-02-16 03:17:58 -0600

    More stylistic changes

--- qwyckems/frontend/front/src/components/Navbar.jsx
+++ qwyckems/frontend/front/src/components/Navbar.jsx
@@ -3,15 +3,26 @@ import "../styles/Navbar.css";
 const Navbar = ({ setCurrentPage }) => {
   return (
     <nav className="navbar">
-      <img src="/qlogocolor.png" alt="QwyckEm's Logo" className="logo" onClick={() => setCurrentPage("home")} />
-      <div className="navbar-title">
-        <h1>QwyckEm's</h1>
-        <p>Pick Em's Done Faster</p>
+      {/* Left Side - Logo */}
+      <div className="navbar-left" onClick={() => setCurrentPage("home")}>
+        <img src="/qlogocolor.png" alt="QwyckEm's Logo" className="logo" />
       </div>
+
+      {/* Center - Stylized Title */}
+      <div className="navbar-center">
+        <h1 className="navbar-title">QwyckEms</h1>
+      </div>
+
+      {/* Right Side - User Profile */}
       <div className="user-profile">
         <img src="/defaultprofile.avif" alt="User Profile" className="profile-pic" />
+        <div className="user-info">
+          <span className="user-name">Roman</span>
+          <span className="user-role">Commissioner</span>
+        </div>
       </div>
     </nav>
+
   );
 };
 
