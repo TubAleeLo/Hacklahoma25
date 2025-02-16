3258c0c2014dc50098e6bc018d24b882aa56f21d
RomanXXII <romanbeames@gmail.com>
2025-02-16 07:36:52 -0600

    Added create/join league framework

--- qwyckems/frontend/front/src/App.jsx
+++ qwyckems/frontend/front/src/App.jsx
@@ -7,7 +7,9 @@ import Week from "./components/Week";
 import Standings from "./components/Standings";
 import Admin from "./components/Admin";
 import SetCorrectPicks from "./components/SetCorrectPicks";
+import { CreateLeague, JoinLeague } from "./components/CreateJoinLeague";
 import "./styles/App.css";
+import "./styles/CreateJoinLeague.css";
 
 const App = () => {
   const [currentPage, setCurrentPage] = useState("home");
@@ -23,14 +25,16 @@ const App = () => {
         <Navbar setCurrentPage={setCurrentPage} />
         <div className="main-container">
           <Sidebar expandedLeague={expandedLeague} toggleLeague={toggleLeague} setCurrentPage={setCurrentPage} />
-          <div className="content">
+          <div className="content"> {/* Keep this to prevent navbar/sidebar from disappearing */}
             {currentPage === "home" && <Home setCurrentPage={setCurrentPage} />}
+            {currentPage === "createLeague" && <CreateLeague setCurrentPage={setCurrentPage} />}
+            {currentPage === "joinLeague" && <JoinLeague setCurrentPage={setCurrentPage} />}
             {currentPage === "week1" && <Week week="1" />}
             {currentPage === "week2" && <Week week="2" />}
             {currentPage === "week3" && <Week week="3" />}
             {currentPage === "standings" && <Standings />}
             {currentPage === "admin" && <Admin />}
-            {currentPage === "setCorrectPicks" && <SetCorrectPicks />} 
+            {currentPage === "setCorrectPicks" && <SetCorrectPicks />}
           </div>
         </div>
       </div>
